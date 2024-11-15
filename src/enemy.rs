cbc4c3d61852a0b88025bd07474c076e1acb1758
Lucas chen <lucas.chen@gbo-asia.com>
2024-11-15 17:31:16 +0800

    add comments

--- src/enemy.rs
+++ src/enemy.rs
@@ -11,6 +11,7 @@ use crate::{
     PLAYER_SPRITE_SCALED_WH, SPRITE_SCALE,
 };
 
+// 敌人相关插件
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
@@ -67,8 +68,9 @@ fn enemy_fire(
         spawn_timer.enemy_laser.tick(time.delta());
 
         let translation = &enemy_tf.translation;
-        let attack_port_x = translation.x;
-        let attack_port_y = translation.y - ENEMY_SPRITE_SCALED_WH.1;
+        // 发生
+        let emit_port_x = translation.x;
+        let emit_port_y = translation.y - ENEMY_SPRITE_SCALED_WH.1;
 
         if spawn_timer.enemy_laser.finished() {
             // 生成敌人镭射
@@ -82,7 +84,7 @@ fn enemy_fire(
                 SpriteBundle {
                     texture: game_textures.enemy_laser.clone(),
                     transform: Transform {
-                        translation: Vec3::new(attack_port_x, attack_port_y, 0.0),
+                        translation: Vec3::new(emit_port_x, emit_port_y, 0.0),
                         scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.0),
                         // rotation: todo
                         ..Default::default()
@@ -96,14 +98,17 @@ fn enemy_fire(
 
 #[allow(clippy::type_complexity)]
 fn enemy_laser_hit_player(
-    mut commands: Commands,
-    laser_query: Query<(Entity, &Transform), (With<Laser>, With<FromEnemy>)>,
-    player_query: Query<(Entity, &Transform), With<Player>>,
+    mut commands: Commands, // 世界指令
+    laser_query: Query<(Entity, &Transform), (With<Laser>, With<FromEnemy>)>, // 镭射子弹查询器
+    player_query: Query<(Entity, &Transform), With<Player>>, // 玩家 (只有一个) 查询器
 ) {
-    // determine if player still alive
+    // 确保玩家还活着
+    // 即玩家实体没有被移除
     if let Ok((player_ent, player_tf)) = player_query.get_single() {
+        // 查询并循环所有敌人 `With<FromEnemy>` 的镭射实体
         for (laser_ent, laser_tf) in laser_query.iter() {
-            let is_collide = Aabb2d::new(
+            // 计算玩家盒子和敌人镭射子弹是否相交来代表是否发生碰撞
+            if Aabb2d::new(
                 laser_tf.translation.truncate(),
                 Vec2::new(
                     ENEMY_LASER_SPRITE_SCALED_WH.0 / 2.0,
@@ -116,9 +121,8 @@ fn enemy_laser_hit_player(
                     PLAYER_SPRITE_SCALED_WH.0 / 2.0,
                     PLAYER_SPRITE_SCALED_WH.1 / 2.0,
                 ),
-            ));
-
-            if is_collide {
+            )) {
+                // 发生碰撞后删除敌人镭射实体和玩家实体
                 // remove the specified laser entity
                 commands.entity(laser_ent).despawn();
                 // remove the player entity
@@ -128,10 +132,13 @@ fn enemy_laser_hit_player(
     }
 }
 
+/// # despawn_enemy 销毁敌人
+///
+/// 当敌人超出屏幕范围时销毁
 fn despawn_enemy(
     mut commands: Commands,
-    query: Query<(Entity, &Transform), With<Enemy>>,
-    window_size: Res<WindowSize>,
+    query: Query<(Entity, &Transform), With<Enemy>>, // 敌人查询器
+    window_size: Res<WindowSize>,                    // 屏幕尺寸资源
 ) {
     let bottom = -window_size.half_height();
 
